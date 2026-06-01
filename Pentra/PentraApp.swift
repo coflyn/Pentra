4b5f9c7d53512b70ad17126ebca7f48b7b0e611f
coflyn <riazrepo@gmail.com>
2026-06-01 08:25:55 +0800

    Initial Commit

---
+++ Pentra/PentraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PentraApp.swift
+//  Pentra
+//
+//  Created by Raffi  on 01/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PentraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PentraTests/PentraTests.swift
@@ -0,0 +1,16 @@
+//
+//  PentraTests.swift
+//  PentraTests
+//
+//  Created by Raffi  on 01/06/26.
+//
+
+import Testing
+
+struct PentraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
